db5b84b944b719e002d44535905c9ecdbe1dbd69
Dicey1625 <plcmnb@icloud.com>
2024-03-11 15:44:55 +1000

    new file:   calc.js 	new file:   calculator.html 	modified:   index.html

---
+++ calc.js
@@ -0,0 +1,6 @@
+function calculate() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let sum12 = num1 + num2;
+    document.getElementById("sum").innerHTML = sum12;
+}
